96b6fbc4e792b516a0559c4f60de117d8acd49a2
Patryk Szczypien <patryk.szczypien@gmail.com>
2021-12-01 22:58:04 +0100

    solution to part 2

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -4,6 +4,23 @@
 
 #include "day1.h"
 #include <chrono>
+#include <iterator>
+
+constexpr auto group_data = [] (const uint32_t amount, const std::vector<uint32_t> &data) {
+    auto remaining_size = data.size();
+    auto iterator = data.begin();
+    std::vector<uint32_t> result_data;
+    while (remaining_size >= amount) {
+        uint32_t sum = *iterator;
+        for (uint32_t i = 1; i < amount; i++) {
+            sum += *std::next(iterator, i);
+        }
+        result_data.push_back(sum);
+        remaining_size--;
+        ++iterator;
+    }
+    return result_data;
+};
 
 constexpr auto count_depth_increase = [] (const std::vector<uint32_t> &data) {
     uint32_t counter = 0;
@@ -17,12 +34,21 @@ constexpr auto count_depth_increase = [] (const std::vector<uint32_t> &data) {
     return counter;
 };
 
+static const std::vector<uint32_t> test_input = {199,
+                                                 200,
+                                                 208,
+                                                 210,
+                                                 200,
+                                                 207,
+                                                 240,
+                                                 269,
+                                                 260,
+                                                 263};
 int main() {
     auto before = std::chrono::high_resolution_clock::now();
     uint32_t depth_increases;
-    for (int i = 0; i < 1000; i++) {
-        depth_increases = count_depth_increase(input);
-    }
+    depth_increases = count_depth_increase(group_data(3, input));
+
     auto after = std::chrono::high_resolution_clock::now() - before;
     std::printf("Counted %d in %lld microseconds", depth_increases, std::chrono::duration_cast<std::chrono::microseconds>(after).count());
     return 0;
