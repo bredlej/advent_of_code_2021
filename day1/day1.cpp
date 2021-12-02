a42cc91f709fc181c30df6cc075069413922b37b
Patryk Szczypien <patryk.szczypien@gmail.com>
2021-12-02 09:58:32 +0100

    changed constexpr lambdas to normal functions

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -6,7 +6,7 @@
 #include <chrono>
 #include <iterator>
 
-constexpr auto group_data = [] (const uint32_t amount, const std::vector<uint32_t> &data) {
+std::vector<uint32_t> group_data(const uint32_t amount, const std::vector<uint32_t> &data) {
     auto remaining_size = data.size();
     auto iterator = data.begin();
     std::vector<uint32_t> result_data;
@@ -22,7 +22,7 @@ constexpr auto group_data = [] (const uint32_t amount, const std::vector<uint32_
     return result_data;
 };
 
-constexpr auto count_depth_increase = [] (const std::vector<uint32_t> &data) {
+uint32_t count_depth_increases(const std::vector<uint32_t> &data) {
     uint32_t counter = 0;
     uint32_t last_depth = data.front();
     for (auto depth: data) {
@@ -34,21 +34,11 @@ constexpr auto count_depth_increase = [] (const std::vector<uint32_t> &data) {
     return counter;
 };
 
-static const std::vector<uint32_t> test_input = {199,
-                                                 200,
-                                                 208,
-                                                 210,
-                                                 200,
-                                                 207,
-                                                 240,
-                                                 269,
-                                                 260,
-                                                 263};
+// static const std::vector<uint32_t> test_input = {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+
 int main() {
     auto before = std::chrono::high_resolution_clock::now();
-    uint32_t depth_increases;
-    depth_increases = count_depth_increase(group_data(3, input));
-
+    uint32_t depth_increases = count_depth_increases(group_data(3, input));
     auto after = std::chrono::high_resolution_clock::now() - before;
     std::printf("Counted %d in %lld microseconds", depth_increases, std::chrono::duration_cast<std::chrono::microseconds>(after).count());
     return 0;
